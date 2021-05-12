fdd8a373350d47f7fa4bd8fbd9b720d7463c8ca4
tereom <teresa.ortiz.mancera@gmail.com>
2021-05-12 18:52:55 -0500

    Agregar explicación de CRS

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 Material para el [taller de mapas con RLadies Montevideo](https://mapas-rladies-mv.netlify.app/).
 
 Prerrequisitos
-* Instalar `usethis`, `tidyverse`, `sf` y `leaflet` (si tienes problemas en este paso abre 
-una cuenta en [rstudio.cloud](https://rstudio.cloud/) y se compartirá el material 
-para el taller por ese medio). 
+
+* Instalar `usethis`, `tidyverse`, `sf` y `leaflet`.
+
+* Descarga el material del curso.
 
 ```
-pkgs <- c("usethis", "tidyverse", "sf", "leaflet")
-install.packages(pkgs)
+install.packages(c("usethis", "tidyverse", "sf", "leaflet"))
+usethis::use_course("https://github.com/tereom/taller-mapas-mv/blob/master/material.zip?raw=true")
 ```
-
 
 * Conveniente conocer el operador pipeline `%>%`.  
 
--- ejercicios.R
+++ ejercicios.R
@@ -4,17 +4,56 @@
 library(tidyverse) # arreglar datos (dplyr) y graficar (ggplot2)
 library(sf) # simple features para trabajar con datos espaciales
 
-## 1. Ejercicios: Montevideo ----
+## 1. Ejercicios: Estáticos ----
 # Grafica los polígonos de barrios de Montevideo
 # archivos en datos/poligonos-uy/ine_barrios_mvd_nbi85
 barrios_mv <- read_sf(#rellena)
 
 ggplot(barrios_mv) +
-  geom_#cambia()
+  geom_#rellena()
 
 #Lee el archivo en `datos/uptu_cultura` y agrega a la misma gráfica que los
 # polígonos.
 cultura_mv <- #rellena
 ggplot() +
-  #rellena
-  #rellena
+  geom_#rellena
+  geom_#rellena
+
+## 2. Ejercicios: Interactivos ----
+library(leaflet)
+# 0. Corre el código
+# 1. Reproyecta los datos cultura_mv para poder usar en leaflet (código 4326)
+cultura_mv_longlat <- st_transform(#rellena, #rellena)
+leaflet(data = cultura_mv_longlat) %>%
+  addTiles() %>%
+  addCircles()
+
+barrios_longlat <- barrios_mv %>%
+  st_transform(crs = 4326)
+
+leaflet(data = barrios_longlat) %>%
+  addTiles() %>%
+  addPolygons()
+
+# datos de ubicaciones RLadies
+library(googlesheets4)
+url <- ("https://docs.google.com/spreadsheets/d/1h7n3dpjqGofWYtKf5ADCDAr8nT5U5UalMFq9nS3G_ew/edit#gid=0")
+gs4_deauth()
+colab <- read_sheet(url)
+# convertimos a objeto sf
+colab_longlat <- colab %>%
+  st_as_sf(coords = c("lat", "long")) %>%
+  st_sf(crs = 4326)
+
+# 2. Agrega al siguiente código la ubicación de los puntos de interés de Montevideo.
+# Recuerda que debes usar datos en long/lat
+leaflet(data = colab_longlat) %>%
+  addTiles() %>%
+  addMarkers(popup = ~nombre) %>%
+  add#rellena(data = #rellena)
+
+# 3. Experimenta con otros fondos, utiliza la función addProviderTiles() ----
+### explora los fondo http://leaflet-extras.github.io/leaflet-providers/preview/index.html
+leaflet(data = colab_longlat) %>%
+  addProviderTiles(providers$#rellena) %>%
+  addMarkers(popup = ~nombre)
